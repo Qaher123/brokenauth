60bb1baf95222d1799c9ce0336b8e5a7015d675a
qaher <2097349@talnet.nl>
2020-11-23 00:04:21 +0100

    dit is de login skeleton

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form class="" action="index.php" method="post">
+
+      <input type="email" name="" value="" placeholder="email" required>
+
+      <br><br>
+
+
+      <input type="password" name="" value="" placeholder="password" required>
+
+      <br><br>
+
+      <input type="submit" name="" value="log in">
+
+    </form>
+
+  </body>
+</html>
